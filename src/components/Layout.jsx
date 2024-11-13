687ff27a7365cebff6c3ca48f3f2297dfbcb7018
KlasniMisho123 <mikheili.silagava.1@btu.edu.ge>
2024-11-13 22:39:40 +0400

    Footer Demo  Commit

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,11 +3,23 @@ export default function Layout(props) {
     const { children } = props
 
     const header = (
-        <header>header</header>
+        <header>
+            <div>
+                <h1 className="text-gradient">ESPRESSO<span className="m-meter">𝔪</span>ETER</h1>
+                <p> For Coffee Insatiates</p>
+            </div>
+            <button> 
+                <p>Sign up free</p>
+                <i className="fa-solid fa-mug-hot"></i>
+            </button>
+        </header>
     )
 
     const footer = (
-        <footer>footer</footer>
+        <footer>
+            <p><span className="text-gradient"> Espresso𝔪eter </span> was inspired by - <a target="_blank" href="https://www.smoljames.com">Smoljames</a> 
+            using the - <a  target="_blank" href="https://www.fantacss.smoljames.com">FantaCSS</a> design library</p>
+        </footer>
     )
 
  return(
