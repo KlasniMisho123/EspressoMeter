2bf4b24a82eeb1adf9972df10b5793fb552c2654
KlasniMisho123 <mikheili.silagava.1@btu.edu.ge>
2024-11-13 22:13:00 +0400

    Hero Commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,12 +5,23 @@ export default function Hero() {
     <>
         <h1> Coffee Tracking for Cofee <abbr title="An Enthusiast or devotee">Fiends</abbr></h1>
         <div className='benefits-list'>
-            <h3 className='font-bolder '> <i className="fa-solid fa-mug-hot"></i> Try <span className='text-gradient'>Espresso𝔪eter</span> and start ...</h3>
+            <h3 className='font-bolder '>Try <span className='text-gradient'>Espresso𝔪eter</span> and start ...</h3>
             <p>✅ Tracking every cofeee </p>
             <p>✅ Measuring your blood caffeine levels </p>
             <p>✅ Costing and quantifying your addiction </p>
-       
-        </div>
+       </div>
+       <div className='card info-card'>
+          <div>
+            <i className="fa-solid fa-circle-info"></i>
+            <h3> Did You Know... </h3>
+            <h5>That caffeine's galf-life is about 5 hours?</h5>
+            <p>This means that after 5 hours, half caffeine you \
+              consumed is still in your system, keeping you alert longer! so 
+              if you drink a cup of coffee with 200 mg if caffeine,
+              5 hours, later, you'll still have about
+               100 mg of caffeine in your system </p>
+          </div>
+       </div>
     </>
   )
 }
