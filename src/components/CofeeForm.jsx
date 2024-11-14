a2898bc2c197c817e9b8646c12905b5a3ffe2e01
KlasniMisho123 <mikheili.silagava.1@btu.edu.ge>
2024-11-14 20:13:11 +0400

    CoffeeForm Commit

--- src/components/CofeeForm.jsx
+++ src/components/CofeeForm.jsx
@@ -1,7 +1,28 @@
 import React from 'react'
+import { coffeeOptions } from '../utils'
 
 export default function CofeeForm() {
   return (
-    <div>CofeeForm</div>
+    <>
+    <div className='section-header'>
+      <i className='fa-solid fa-pencil'></i>
+        <h2>Start Tracking Today</h2>
+    </div>
+    <h4>Select coffee type</h4>
+    <div className='coffee-grid'>
+        { coffeeOptions.slice(0, 5).map((option, optionIndex) => {
+          return(
+          <button className='button-card' key={{optionIndex}}>
+            <h4>{option.name}</h4>
+            <p>{option.caffeine} mg</p>
+          </button>
+          )
+        })}
+        <button className="button-card">
+            <h4> Other </h4>
+            <p> n/a</p>
+        </button>
+    </div>
+    </>  
   )
 }
