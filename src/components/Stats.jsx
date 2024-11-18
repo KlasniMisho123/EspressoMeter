785d579984ca769cdf3873da0f7e37c543211d3e
KlasniMisho123 <mikheili.silagava.1@btu.edu.ge>
2024-11-18 21:56:25 +0400

    Stats Commit

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { calculateCurrentCaffeineLevel, coffeeConsumptionHistory, statusLevels } from '../utils'
 
 function StatCard(props) {
   const {lg, title, children } = props
@@ -12,6 +13,15 @@ function StatCard(props) {
 }
 
 export default function Stats() {
+  const stats = {
+    daily_caffeine:240,
+    daily_cost: 6.8,
+    average_coffees: 2.3,
+    total_cost: 220
+  }
+
+  const caffeineLevel = calculateCurrentCaffeineLevel(coffeeConsumptionHistory)
+
   return (
     <>
       <div className='selection-header'>
@@ -19,7 +29,13 @@ export default function Stats() {
         <h2>Stats</h2>
       </div>
       <div className='stats-grid'>
-          <StatCard lg title="Active Caffeine Level"></StatCard>
+          <StatCard lg title="Active Caffeine Level">
+            <div className='status'>
+              <p><span className='stat-text'>{caffeineLevel}</span>mg</p>
+              <h5 style={{color: statusLevels["low"].color, background: statusLevels["low"].background}}>Low</h5>
+              <p>{statusLevels['low'].description}</p>
+            </div>
+          </StatCard>
           <StatCard title="Daily Caffeine"></StatCard>
           <StatCard title="Avg # of Coffees"></StatCard>
           <StatCard title="Daily Cost ($) "></StatCard>
