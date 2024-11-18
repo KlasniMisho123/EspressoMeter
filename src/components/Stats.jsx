d8590ee260efbed572fbe856166b3e64e1af8073
KlasniMisho123 <mikheili.silagava.1@btu.edu.ge>
2024-11-18 21:34:48 +0400

    Stats Commit

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,7 +1,30 @@
 import React from 'react'
 
+function StatCard(props) {
+  const {lg, title, children } = props
+
+    return(
+      <div className={'card start-card '  + (lg ? ' col-span-2' : "")}>
+        <h4> {title} </h4>
+        {children}
+      </div>
+    )
+}
+
 export default function Stats() {
   return (
-    <div>Stats</div>
+    <>
+      <div className='selection-header'>
+        <i className='fa-solid fa-chart-simple'/>
+        <h2>Stats</h2>
+      </div>
+      <div className='stats-grid'>
+          <StatCard lg title="Active Caffeine Level"></StatCard>
+          <StatCard title="Daily Caffeine"></StatCard>
+          <StatCard title="Avg # of Coffees"></StatCard>
+          <StatCard title="Daily Cost ($) "></StatCard>
+          <StatCard title="Total Cost ($) "></StatCard>
+      </div>
+    </>
   )
 }
