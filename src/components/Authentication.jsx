1ba964fe8338cc401750e3dd3cbe07b26412d7f5
KlasniMisho123 <mikheili.silagava.1@btu.edu.ge>
2024-11-19 16:15:55 +0400

    auth Form Commit

--- src/components/Authentication.jsx
+++ src/components/Authentication.jsx
@@ -2,6 +2,17 @@ import React from 'react'
 
 export default function Authentication() {
   return (
-    <div>Authentication</div>
+    <>
+    <h2 className='sign-up-text'>Sign up / Login</h2>
+    <p>Sign in to your accaunt</p>
+    <input placeholder='Email'/>
+    <input placeholder='********' type='password' />
+    <button> <p>Submit </p> </button>
+    <hr/>
+    <div className='regster-content'>
+      <p>Don't have an accaunt?</p>
+      <button><p>Sign up</p></button>
+    </div>
+    </>
   )
 }
