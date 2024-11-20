e73684e0e322b5f7a33b29c28be0d1a52288334c
KlasniMisho123 <mikheili.silagava.1@btu.edu.ge>
2024-11-20 20:07:02 +0400

    Firebase commit

--- src/components/Authentication.jsx
+++ src/components/Authentication.jsx
@@ -5,7 +5,7 @@ export default function Authentication() {
   const [isRegistration, setIsRegistration] = useState(false)
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
-  const [Authenticating, setAuthenticating] = useState(false)
+  const [authenticating, setAuthenticating] = useState(false)
 
   async function handleAuthentication() {
     
@@ -14,7 +14,7 @@ export default function Authentication() {
   return (
     <>
     <h2 className='sign-up-text'>{isRegistration ? "Sign up" : "Login"}</h2>
-    <p>{isRegistration ? "Register your account" : "Login to your accaunt"} </p>
+    <p>{isRegistration ? "Create an account" : "Sign in to your accaunt"} </p>
     <input value={email} onchange={(e)=>{setEmail(e.target.value)}} placeholder='Email'/>
     <input value={password} onchange={(e)=>{setPassword(e.target.value)}} placeholder='********' type='password' />
     <button onClick={handleAuthentication}> <p>Submit </p> </button>
@@ -23,7 +23,7 @@ export default function Authentication() {
       <p>{isRegistration ? "Already have account?" : "Don't have an account?"}</p>
       <button onClick={()=> {setIsRegistration(!isRegistration) }}>
         <p>
-        {isRegistration ? "LogIn " : "Sign Up"}
+        {isRegistration ? "Sign in " : "Sign Up"}
         </p>
         </button>
     </div>
