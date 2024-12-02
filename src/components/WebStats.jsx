cb394bc2278354e67d3948604e610718e18f942f
KlasniMisho123 <mikheili.silagava.1@btu.edu.ge>
2024-12-02 18:00:42 +0400

    WebStats Commit

--- src/components/WebStats.jsx
+++ src/components/WebStats.jsx
@@ -1,7 +1,33 @@
 import React from 'react'
+import { db } from '../../firebase'
 
-export default function WebStats() {
+export default function WebStats(props) {
+    const {} = props
   return (
-    <div>WebStats</div>
+    <div className='web-stats-layout'>
+        <div className='web-stats-grid'>
+            <div className='web-stats-div one'>
+                <i className="fa-solid fa-users"></i>
+                <div> 170 + </div>
+                <h5> Total Users </h5>
+            </div>
+            <div className='web-stats-div two'>
+                <i className="fa-solid fa-users"></i>
+                <div> 5778 + </div>
+                <h5> Commits </h5>
+            </div>
+
+            <div className='web-stats-div three'>
+                <i className="fa-solid fa-users"></i>
+                <div> 20 + </div>
+                <h5> Demo Stats </h5>
+                </div>
+            <div className='web-stats-div four'>
+                <i className="fa-solid fa-users"></i>
+                <div> 12 + </div>
+                <h5> Demo Stats </h5>
+            </div>
+        </div>
+    </div>
   )
 }
